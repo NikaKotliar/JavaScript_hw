7da2213e9f81fea368f0fbe71016610bfb009065
Nika Kotliar <wanderer2805@gmail.com>
2022-08-17 11:56:53 +0300

    menu fixed

--- js-features/2/menu/1.js
+++ js-features/2/menu/1.js
@@ -5,13 +5,20 @@ menuItems.forEach(function (menuItem) {
     if (menuItem.querySelector('ul')) {
         menuItem.querySelector("a").onclick = () => { return false }
         menuItem.addEventListener('click', () => {
-            closeMenu()
-            menuItem.querySelector("ul").classList.toggle('menu_active')
-
+            
+            if (menuItem.querySelector("ul").classList.contains("menu_active")) {
+                // menuItem.querySelector("ul").classList.remove('menu_active')
+                 closeMenu()
+            } else {
+                closeMenu()
+                menuItem.querySelector("ul").classList.add('menu_active')
+            }
         })
+
     }
-}
-)
+})
+
+
 
 
 function closeMenu() {
