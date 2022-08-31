e1ebe62b8e52ef42be5a637bf7dbfb8189164047
Nika Kotlyar <wanderer2805@gmail.com>
2022-08-31 18:32:10 +0300

    second decided

--- js-features/6_document-structure/todo/1.js
+++ js-features/6_document-structure/todo/1.js
@@ -1,37 +1,71 @@
+const inputUser = document.querySelector('.tasks__input')
+const addButton = document.getElementById('tasks__add')
 
 function getUserInput() {
-    const inputUser = document.querySelector('.tasks__input')
+    if (inputUser.value) {
+        createNewTask(inputUser.value)
+        inputUser.value = ''
+    } else {
+        alert('Вы не ввели название задачи')
+    }
+}
+
+function catchNewTask() {
+    addButton.addEventListener('click', () => {
+        getUserInput()
+        tasksControl() 
+    })
     inputUser.addEventListener('keyup', (e) => {
-        console.log(inputUser.value)
-        if (e.code == "Enter") {
-            if (inputUser.value) {
-                console.log(inputUser.value)
-                const inputText = inputUser.value
-                return inputText
-            } else {
-                alert('Вы не ввели название новой зададачи')
-            }
+        e.preventDefault()
+        if (e.code == 'Enter') {
+            getUserInput()
+            tasksControl() 
         }
     })
+}
 
+function createNewTask(textTask) {
+
+    const task = document.createElement('div')
+    task.classList.add('task')
+
+    const taskTitle = document.createElement('div')
+    taskTitle.classList.add('task__title')
+    taskTitle.textContent = textTask
+
+    const link = document.createElement('a')
+    link.href = '#'
+    link.innerHTML = '&times;'
+    link.classList.add('task__remove')
+
+    task.append(taskTitle, link)
+
+    const taskList = document.querySelector('.tasks__list')
+    taskList.append(task)
 }
 
 
-// создать под каждое наименование из инпут новый элемент и вставить его в tasks__list
-
-function createNewTask() {
-    console.log('NewTask()')
-    inputText = getUserInput()
-
-
-
-}
-
-createNewTask()
 
 // при нажатии на элемент с классом task__remove, задача должна удаляться
 
+function deleteTask(elem) {
+    elem.remove()
+}
+
+function tasksControl() {
+    let taskRemoves = document.getElementsByClassName('task__remove')
+    for (var i = 0; i < taskRemoves.length; ++i) {
+        let taskRemove = taskRemoves[i]
+        taskRemove.parentElement.onclick = () => {
+            taskRemove.parentElement.remove()
+        }
+    }
+ 
+
+}
 
 
+catchNewTask()
+tasksControl()
 
 
