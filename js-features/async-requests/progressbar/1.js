3dd7ddc8961e868b436dbeb49004e718c1138234
Nika Kotliar <wanderer2805@gmail.com>
2022-09-10 09:48:52 +0300

    third hardly decided

--- js-features/async-requests/progressbar/1.js
+++ js-features/async-requests/progressbar/1.js
@@ -1,3 +1,43 @@
-const progress = document.getElementById( 'progress' );
-progress.value = 0.7;
+const progress = document.getElementById('progress')
 
+
+function onSubmit() {
+    console.log("Вызвана функция")
+
+    let file = document.getElementById("file").files[0]
+    progress.max = file.size
+    console.log(file.size)
+
+    let xhr = new XMLHttpRequest()
+
+    xhr.open('POST', "https://netology-slow-rest.herokuapp.com/upload.php")
+
+    let formData = new FormData()
+    formData.append('file', file)
+
+    xhr.send(formData)
+
+    // xhr.onreadystatechange = function (e) {
+    //     console.log(this.readyState)
+    // }
+
+    xhr.onload = function() {
+        console.log('Успешно загружено', this.status)
+      }
+
+
+    xhr.onprogress = function (e) {
+        this.readyState
+        console.log('onprogress', e)
+        // progress.max = e.total
+        progress.value = e.loaded
+
+    }
+
+    return false
+}
+
+document.getElementById("send").addEventListener("click", () => {
+
+    onSubmit()
+})
