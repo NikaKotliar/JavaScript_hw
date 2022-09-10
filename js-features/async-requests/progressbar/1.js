f73fbf09657b2e73970855183fc8bea63fb0fe8c
Nika Kotliar <wanderer2805@gmail.com>
2022-09-10 20:19:52 +0300

    third corrected

--- js-features/async-requests/progressbar/1.js
+++ js-features/async-requests/progressbar/1.js
@@ -5,10 +5,28 @@ function onSubmit() {
     console.log("Вызвана функция")
 
     let file = document.getElementById("file").files[0]
-    progress.max = file.size
-    console.log(file.size)
 
     let xhr = new XMLHttpRequest()
+
+
+    // xhr.onreadystatechange = function (e) {
+    //     console.log(this.readyState)
+    // }
+
+
+    xhr.upload.onprogress = function (e) {
+        this.readyState
+        console.log('onprogress', e)
+        progress.max = e.total
+        progress.value = e.loaded
+        console.log(e.loaded)
+
+    }
+
+
+    xhr.upload.onload = function () {
+        console.log('Успешно загружено', this.status)
+    }
 
     xhr.open('POST', "https://netology-slow-rest.herokuapp.com/upload.php")
 
@@ -17,22 +35,6 @@ function onSubmit() {
 
     xhr.send(formData)
 
-    // xhr.onreadystatechange = function (e) {
-    //     console.log(this.readyState)
-    // }
-
-    xhr.onload = function() {
-        console.log('Успешно загружено', this.status)
-      }
-
-
-    xhr.onprogress = function (e) {
-        this.readyState
-        console.log('onprogress', e)
-        // progress.max = e.total
-        progress.value = e.loaded
-
-    }
 
     return false
 }
