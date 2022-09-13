7e5de03e6e8b197df4bf01e49d4b4ab6ff146abf
Nika Kotliar <wanderer2805@gmail.com>
2022-09-13 10:10:25 +0300

    11

--- js-features/client-state/auth/1.js
+++ js-features/client-state/auth/1.js
@@ -4,26 +4,24 @@
 
 
 const url = 'https://netology-slow-rest.herokuapp.com/auth.php'
-
-let data = {
-    login: "demo",
-    password: "demo"
-}
+const form = document.forms[0]
 
 
-function sendData() {
+function sendData(login, password) {
+
+    let formData = new FormData();
+    formData.append('login', login);
+    formData.append('password', password);
 
     let fetchData = {
         method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-type': 'application/json;',
-        }
+        body: formData,
     }
 
     fetch(url, fetchData)
-        .then((response) =>
-            response.json())
+        .then((response) =>{
+            response.json()
+        })
         .then(function (data) {
             console.log(data)
         })
@@ -33,4 +31,50 @@ function sendData() {
 
 }
 
-sendData()
+
+//Получить данные с формы
+function getInfoFromForm() {
+    let login = form.elements.login.value
+    let password = form.elements.password.value
+
+    return [login, password]
+}
+
+//Авторизовать пользователя или показать ошибку
+function IsOk(ok) {
+    if (ok) {
+        onSuccessAuth()
+    } else {
+        alert('Неверные логин/пароль')
+    }
+}
+
+
+// Перерисовать экран входа
+function onSuccessAuth(userName) {
+    document.querySelector(".signin").classList.remove("signin_active")
+    welcomeText = document.getElementById("welcome")
+    welcomeText.classList.add("welcome_active")
+    welcomeText.innerText = 'Добро пожаловать, пользователь ' + userName
+
+}
+
+//сохранить инфо о залогиненом пользователя
+
+
+//проверка есть ли пользователь и показ экрана входа
+
+//инициация отправки формы
+function onSubmit(){
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        let datas = getInfoFromForm()
+        login = datas[0]
+        password = datas[1]
+
+        sendData(login,password)
+
+    })
+}
+
+onSubmit()
